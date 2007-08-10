cf5e1dda45cd13f63e37c3b57302276a7f0e3708
ekeeke31 <ekeeke31@fcb46045-4337-0410-a722-cd382f7ce8f6>
2007-08-10 19:31:42 +0000

    Removed file/folder

---  genplus-gx/source/memvdp.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef _MEMVDP_H_
-#define _MEMVDP_H_
-
-/* Function prototypes */
-unsigned int vdp_dma_r (unsigned int address);
-
-#endif /* _MEMVDP_H_ */
---  genplus-gx/source/unused/win/main.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-
-
-#endif /* _MAIN_H_ */
