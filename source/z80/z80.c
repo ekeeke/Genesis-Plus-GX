cd14774e03beb4df40ff91f0bcb53574f12ef281
ekeeke31 <ekeeke31@fcb46045-4337-0410-a722-cd382f7ce8f6>
2010-08-08 17:28:11 +0000

    [Z80] fixed state of CPU registers on reset: fixes missing sound in Defender/Defender 2 (Williams Arcade Classics)

--- source/z80/z80.c
+++ source/z80/z80.c
@@ -3353,6 +3353,9 @@ void z80_init(const void *config, int (*irqcallback)(int))
   }
 
   /* Reset registers to their initial values */
+  memset(&Z80, 0, sizeof(Z80));
+  IX = IY = 0xffff; /* IX and IY are FFFF after a reset! */
+  F = ZF;      /* Zero flag is set */
   Z80.daisy = config;
   Z80.irq_callback = irqcallback;
 
@@ -3370,25 +3373,19 @@ void z80_init(const void *config, int (*irqcallback)(int))
  ****************************************************************************/
 void z80_reset(void)
 {
-  /* save previous values */
-  void *config = (void *) Z80.daisy;
-  int (*irqcallback)(int) = Z80.irq_callback;
-
-  /* Reset registers to their initial values */
-  memset(&Z80, 0, sizeof(Z80));
-  Z80.daisy = config;
-  Z80.irq_callback = irqcallback;
-  IX = IY = 0xffff; /* IX and IY are FFFF after a reset! */
-  F = ZF;      /* Zero flag is set */
-
   PC = 0x0000;
   I = 0;
   R = 0;
   R2 = 0;
+  IM = 0;
+  IFF1 = IFF2 = 0;
+  HALT = 0;
+
   Z80.nmi_state = CLEAR_LINE;
   Z80.nmi_pending = FALSE;
   Z80.irq_state = CLEAR_LINE;
   Z80.after_ei = FALSE;
+
   WZ=PCD;
 }
 
