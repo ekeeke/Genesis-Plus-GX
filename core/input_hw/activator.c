660d7ea9d1c4507c970bb6c8dba1b0a8b942a32c
EkeEke <ekeeke31@gmail.com>
2013-06-27 01:54:02 +0200

    [Core/input] updated header of modified files

--- core/input_hw/activator.c
+++ core/input_hw/activator.c
@@ -2,7 +2,7 @@
  *  Genesis Plus
  *  Sega Activator support
  *
- *  Copyright (C) 2011  Eke-Eke (Genesis Plus GX)
+ *  Copyright (C) 2011-2013  Eke-Eke (Genesis Plus GX)
  *
  *  Redistribution and use of this code or any derivative works are permitted
  *  provided that the following conditions are met:
