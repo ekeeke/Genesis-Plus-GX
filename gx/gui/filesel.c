f436689a654604a1b3e1fb43e321671289d1cc40
EkeEke <ekeeke31@gmail.com>
2014-09-20 18:01:14 +0200

    [Gamecube/Wii] fixed regression with recent file list display (continued)

--- gx/gui/filesel.c
+++ gx/gui/filesel.c
@@ -231,7 +231,7 @@ static void selector_cb(void)
       }
       else
       {
-        FONT_write(filelist[i].filename,18,dir_icon.x,yoffset+22,bar_over.w-20,(GXColor)WHITE);
+        FONT_write(filelist[i].filename,18,26,yoffset+22,bar_over.w-20,(GXColor)WHITE);
       }
     }
 
