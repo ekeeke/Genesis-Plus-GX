fd06e074bb6d9836db8f7f6dbca5c3a2f6a050c8
Gregor Richards <hg-yff@gregor.im>
2016-12-09 18:07:12 -0500

    Report git version with library_version

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -2,6 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+	LOCAL_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
 WANT_CRC32 := 1
 
 CORE_DIR := ../..
