8d14bf479dab097b774abea176e20283fbb1d383
twinaphex <libretro@gmail.com>
2017-06-25 00:28:54 +0200

    Update

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -26,7 +26,7 @@ include $(LIBRETRO_DIR)/Makefile.common
 LOCAL_SRC_FILES := $(SOURCES_C)
 
 LOCAL_C_INCLUDES = $(foreach dir,$(GENPLUS_SRC_DIR),$(LOCAL_PATH)/$(dir)) \
-			$(LOCAL_PATH)/libretro-common/include \
+			$(CORE_DIR)/libretro-common/include \
 			$(LOCAL_PATH)/$(TREMOR_SRC_DIR) \
 			$(LOCAL_PATH)/$(LIBRETRO_DIR)
 
