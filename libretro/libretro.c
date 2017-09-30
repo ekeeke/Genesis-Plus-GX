388f73d658c7564d363a1f48f49b5e3fcecfdeb4
ekeeke <ekeeke31@gmail.com>
2017-09-30 09:58:09 +0200

    [libretro] cosmetic changes to core settings

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -1672,8 +1672,8 @@ void retro_set_environment(retro_environment_t cb)
       { "genesis_plus_gx_ym3438", "YM2612/YM3438 core; mame|nuked (ym2612)|nuked (asic ym3438)|nuked (discrete ym3438)" },
       #endif
 
+      { "genesis_plus_gx_sound_output", "Sound output; stereo|mono" },
       { "genesis_plus_gx_audio_filter", "Audio filter; disabled|low-pass" },
-      { "genesis_plus_gx_sound_output", "Sound output; stereo|mono" }, 
       { "genesis_plus_gx_lowpass_range", "Low-pass filter %; 60|65|70|75|80|85|90|95|5|10|15|20|25|30|35|40|45|50|55"},
       
       #if HAVE_EQ     
