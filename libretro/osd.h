77815779203c55de0173cdf69abae97e10b7bf16
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2020-05-15 20:18:26 -0400

    Fix libretro build for GCC 10

--- libretro/osd.h
+++ libretro/osd.h
@@ -87,7 +87,7 @@ typedef struct
   uint8 padtype;
 } t_input_config;
 
-struct 
+typedef struct
 {
   char version[16];
   uint8 hq_fm;
@@ -129,7 +129,9 @@ struct
   uint8 gun_cursor;
   uint32 overclock;
   uint8 no_sprite_limit;
-} config;
+} t_config;
+
+extern t_config config;
 
 extern char GG_ROM[256];
 extern char AR_ROM[256];
