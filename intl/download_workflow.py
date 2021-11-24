4328cf4c07d4d57968b1318f7d1882ec64252502
Michael Burgardt <michael.burgardt@gmail.com>
2021-11-24 17:42:39 +0100

    Fix translation download & Sync weekly instead of daily (#274)

--- intl/download_workflow.py
+++ intl/download_workflow.py
@@ -11,5 +11,6 @@ except IndexError as e:
     print('Please provide path to libretro_core_options.h, Crowdin API Token and core name!')
     raise e
 
+subprocess.run(['python3', 'intl/crowdin_prep.py', dir_path, core_name])
 subprocess.run(['python3', 'intl/crowdin_translation_download.py', api_key, core_name])
 subprocess.run(['python3', 'intl/crowdin_translate.py', dir_path, core_name])
--- intl/initial_sync.py
+++ intl/initial_sync.py
@@ -85,11 +85,11 @@ if __name__ == '__main__':
             workflow_config = workflow.read()
         workflow_config = workflow_config.replace(
             "subprocess.run(['python3', 'intl/core_option_translation.py', dir_path, core_name])",
-            "subprocess.run(['python3', 'intl/crowdin_translation_download.py', api_key, core_name])"
+            "subprocess.run(['python3', 'intl/crowdin_prep.py', dir_path, core_name])"
         )
         workflow_config = workflow_config.replace(
             "subprocess.run(['python3', 'intl/initial_sync.py', api_key, core_name])\n",
-            ""
+            "subprocess.run(['python3', 'intl/crowdin_translation_download.py', api_key, core_name])"
         )
         with open('intl/translation_workflow.py', 'w') as workflow:
             workflow.write(workflow_config)
