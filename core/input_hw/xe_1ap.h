605a29d7bd45af92d907d0ce869ec95615df1b9f
ekeeke <ekeeke31@gmail.com>
2023-02-05 18:08:05 +0100

    [Core/Input] improved XE-1AP controller emulation (verified on real hardware)

--- core/input_hw/xe_1ap.h
+++ core/input_hw/xe_1ap.h
@@ -2,7 +2,7 @@
  *  Genesis Plus
  *  XE-1AP analog controller support
  *
- *  Copyright (C) 2011-2015  Eke-Eke (Genesis Plus GX)
+ *  Copyright (C) 2011-2022  Eke-Eke (Genesis Plus GX)
  *
  *  Redistribution and use of this code or any derivative works are permitted
  *  provided that the following conditions are met:
