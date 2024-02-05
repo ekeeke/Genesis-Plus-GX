904613f51c7988f326663251efb409d0ceb41a84
ekeeke <ekeeke31@gmail.com>
2024-02-06 00:27:25 +0100

    [Core/CD] added CDC decoder processing when disc is not playing (fixes MCD-verificator CDC Flags Test 0x30)

--- core/cd_hw/cdd.c
+++ core/cd_hw/cdd.c
@@ -2,7 +2,7 @@
  *  Genesis Plus
  *  CD drive processor & CD-DA fader
  *
- *  Copyright (C) 2012-2023  Eke-Eke (Genesis Plus GX)
+ *  Copyright (C) 2012-2024  Eke-Eke (Genesis Plus GX)
  *
  *  Redistribution and use of this code or any derivative works are permitted
  *  provided that the following conditions are met:
@@ -1812,9 +1812,14 @@ void cdd_update(void)
       scd.regs[0x36>>1].byte.h = 0x01;
     }
   }
+  else
+  {
+    /* CDC decoder is still running while disc is not being read (fixes MCD-verificator CDC Flags Test #30) */
+    cdc_decoder_update(0);
+  }
 
   /* scanning disc */
-  else if (cdd.status == CD_SCAN)
+  if (cdd.status == CD_SCAN)
   {
     /* current track index */
     int index = cdd.index;
--- core/cd_hw/cdd.h
+++ core/cd_hw/cdd.h
@@ -2,7 +2,7 @@
  *  Genesis Plus
  *  CD drive processor & CD-DA fader
  *
- *  Copyright (C) 2012-2023  Eke-Eke (Genesis Plus GX)
+ *  Copyright (C) 2012-2024  Eke-Eke (Genesis Plus GX)
  *
  *  Redistribution and use of this code or any derivative works are permitted
  *  provided that the following conditions are met:
