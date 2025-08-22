4f222dd5c5e1730f72034205c347995109812188
ekeeke <ekeeke31@gmail.com>
2025-08-22 13:54:50 +0200

    [Core/MD] fixed support for "SSF header-patched version" of Daemon Claw

--- core/cart_hw/md_cart.c
+++ core/cart_hw/md_cart.c
@@ -539,9 +539,10 @@ void md_cart_init(void)
 
   /* detect specific mappers */
   else if (strstr(rominfo.international,"DEMONS OF ASTEBORG") ||
-           strstr(rominfo.international,"ASTEBROS"))
+           strstr(rominfo.international,"ASTEBROS") ||
+           strstr(rominfo.domestic,"DAEMON CLAW"))
   {
-    /* default SSF2+SRAM mapper (fixes Steam versions which incorrectly use Everdrive extended SSF mapper indicator in ROM header) */ 
+    /* default SSF2+SRAM mapper (fixes patched versions incorrectly indicating Everdrive extended SSF mapper in ROM header) */ 
     cart.hw.time_w = default_time_w;
   }
   else if (strstr(rominfo.consoletype,"SEGA SSF"))
