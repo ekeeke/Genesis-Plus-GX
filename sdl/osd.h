2fa79c1c643375662543afe9e1299a3cf1d55559
ekeeke <ekeeke31@gmail.com>
2025-09-05 10:18:42 +0200

    [SDL] revert previous commit debugging modifications (should not have been pushed upstream)

--- sdl/osd.h
+++ sdl/osd.h
@@ -17,17 +17,17 @@
 
 #define osd_input_update sdl_input_update
 
-#define GG_ROM      "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/ggenie.bin"
-#define AR_ROM      "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/areplay.bin"
-#define SK_ROM      "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/sk.bin"
-#define SK_UPMEM    "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/sk2chip.bin"
-#define CD_BIOS_US  "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_CD_U.bin"
-#define CD_BIOS_EU  "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_CD_E.bin"
-#define CD_BIOS_JP  "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_CD_J.bin"
-#define MD_BIOS     "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_MD.bin"
-#define MS_BIOS_US  "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_U.sms"
-#define MS_BIOS_EU  "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_E.sms"
-#define MS_BIOS_JP  "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios_J.sms"
-#define GG_BIOS     "C:/devkitPro/msys2/home/Genesis-Plus-GX-github/sdl/bios.gg"
+#define GG_ROM      "./ggenie.bin"
+#define AR_ROM      "./areplay.bin"
+#define SK_ROM      "./sk.bin"
+#define SK_UPMEM    "./sk2chip.bin"
+#define CD_BIOS_US  "./bios_CD_U.bin"
+#define CD_BIOS_EU  "./bios_CD_E.bin"
+#define CD_BIOS_JP  "./bios_CD_J.bin"
+#define MD_BIOS     "./bios_MD.bin"
+#define MS_BIOS_US  "./bios_U.sms"
+#define MS_BIOS_EU  "./bios_E.sms"
+#define MS_BIOS_JP  "./bios_J.sms"
+#define GG_BIOS     "./bios.gg"
 
 #endif /* _OSD_H_ */
