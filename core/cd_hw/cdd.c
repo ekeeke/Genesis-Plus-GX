865c02fd4cfb2ff30517bf7c133bde99105eb3e9
ekeeke <ekeeke31@gmail.com>
2026-07-13 15:10:43 +0200

    [Core/CDD] added CDC decoder processing during CD latency (fixes MCD-verificator CDC INIT Test #2 when higher CDD latency is emulated)

--- core/cd_hw/cdd.c
+++ core/cd_hw/cdd.c
@@ -2,7 +2,7 @@
  *  Genesis Plus
  *  CD drive processor & CD-DA fader
  *
- *  Copyright (C) 2012-2025  Eke-Eke (Genesis Plus GX)
+ *  Copyright (C) 2012-2026  Eke-Eke (Genesis Plus GX)
  *
  *  Redistribution and use of this code or any derivative works are permitted
  *  provided that the following conditions are met:
@@ -1778,6 +1778,9 @@ void cdd_update(void)
   if (cdd.latency > 0)
   {
     cdd.latency--;
+
+    /* CDC decoder is still running while disc is seeking (fixes MCD-verificator CDC Init when higher latency is emulated) */
+    cdc_decoder_update(0);
   }
   else
   {
